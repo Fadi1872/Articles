a846114a453e41c384db02c74d02cb84676d26de
hala <hala59779@gmail.com>
2024-06-03 12:01:28 +0300

    roles commit

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -42,20 +42,16 @@
 
                     <div class="mb-3 row">
                         <label for="permissions" class="col-md-4 col-form-label text-md-end text-start">Permissions</label>
-                        <div class="col-md-6">           
-                            <select class="form-select @error('permissions') is-invalid @enderror" multiple aria-label="Permissions" id="permissions" name="permissions[]" style="height: 210px;">
-                                @forelse ($permissions as $permission)
-                                    <option value="{{ $permission->id }}" {{ in_array($permission->id, $rolePermissions ?? []) ? 'selected' : '' }}>
-                                        {{ $permission->name }}
-                                    </option>
-                                @empty
-
-                                @endforelse
-                            </select>
+                        <div class="col-md-6"> 
+                      @forelse ($permissions as $permission)
+                         <input type="checkbox" id="permissions" name="permissions[]" value="{{ $permission->id }}" {{ in_array($permission->id, old('permissions') ?? []) ? 'selected' : '' }}>
+                         {{ $permission->name }}
+                          @empty
+                      @endforelse
                             @if ($errors->has('permissions'))
                                 <span class="text-danger">{{ $errors->first('permissions') }}</span>
                             @endif
-                        </div>
+                    </div>
                     </div>
                     
                     <div class="mb-3 row">
