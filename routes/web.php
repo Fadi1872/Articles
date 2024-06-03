ec63953e482873de30ff7c7ae8573c8c9dd02ec4
hala <hala59779@gmail.com>
2024-06-03 11:55:53 +0300

    role commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::resources([
+    'roles' => RoleController::class,
+]);
