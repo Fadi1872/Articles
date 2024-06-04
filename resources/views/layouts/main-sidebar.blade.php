40be587d622e0766f94a504767f8bf48278346da
Fadi1872 <fadinoumih18@gmail.com>
2024-06-04 16:47:53 +0300

    user list&create

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -25,19 +25,19 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon bi bi-circle-fill"></i>
                         <p>
-                            Books
+                            Users
                             <i class="nav-arrow bi bi-chevron-right"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="{{route('user.index')}}" class="nav-link">
                                 <i class="nav-icon bi bi-circle"></i>
                                 <p>Show</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="{{route('user.create')}}" class="nav-link">
                                 <i class="nav-icon bi bi-circle"></i>
                                 <p>Add New</p>
                             </a>
